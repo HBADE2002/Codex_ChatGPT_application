7301616f1d2a859655af06b79438f244f531f35f
HRISHIKESH BADE <78660475+HBADE2002@users.noreply.github.com>
2023-01-23 13:07:08 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async(e) => {
   loader(messageDiv);
 
   //fetch data from the server -> bots response
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://codex-chatgpt-application.onrender.com',{
     method: 'POST',
     headers:{
       'Content-Type':'application/json'
